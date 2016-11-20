107c98b4290abe5857bf4fa32adfa3a107df4508
Greg Raven <soapy-smith@comcast.net>
2016-11-20 12:58:18 +0000

    More updates to PRU1

--- sw/prupid/PRU_IO_1.c
+++ sw/prupid/PRU_IO_1.c
@@ -33,14 +33,14 @@
 
 #include <stdint.h>
 #include <limits.h>
-#include <pru_cfg.h>
-#include <pru_ecap.h>
-#include <sys_pwmss.h>
-#include <pru_intc.h>
+#include <am335x/pru_cfg.h>
+#include <am335x/pru_ecap.h>
+#include <am335x/sys_pwmss.h>
+#include <am335x/pru_intc.h>
 #include <rsc_types.h>
 #include <pru_virtqueue.h>
 #include <pru_rpmsg.h>
-#include <sys_mailbox.h>
+//#include <sys_mailbox.h>
 #include "resource_table_1.h"
 
 /* Shared PID data structure - ensure both shared stucts match PRU 0 */
@@ -83,20 +83,17 @@ volatile register uint32_t __R31;
 #define TICKS_PER_REV       16
 #define SAMPLES_PER_SEC     12
 #define SEC_PER_MIN         60
-/* PRU1 is mailbox module user 2 */
-#define MB_USER             2
-/* Mbox0 - mail_u2_irq (mailbox interrupt for PRU1) is Int Number 59 */
-#define MB_INT_NUMBER       59
 
 /* Host-1 Interrupt sets bit 31 in register R31 */
 #define HOST_INT            0x80000000
 
-/* The mailboxes used for RPMsg are defined in the Linux device tree
- * PRU0 uses mailboxes 2 (From ARM) and 3 (To ARM)
- * PRU1 uses mailboxes 4 (From ARM) and 5 (To ARM)
- */
-#define MB_TO_ARM_HOST      5
-#define MB_FROM_ARM_HOST    4
+//  The PRU-ICSS system events used for RPMsg are defined in the Linux device
+//  tree.
+//  PRU0 uses system event 16 (to ARM) and 17 (from ARM)
+//  PRU1 uses system event 18 (to ARM) and 19 (from ARM)
+#define TO_ARM_HOST 18
+#define FROM_ARM_HOST 19
+
 /*
  * The name 'rpmsg-pru' corresponds to the rpmsg_pru driver found
  * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
@@ -113,7 +110,7 @@ volatile register uint32_t __R31;
 void init_pwm();
 void init_eqep();
 int get_enc_rpm();
-void init_rpmsg(struct pru_rpmsg_transport *transport);
+void init_rpmsg(struct pru_rpmsg_transport* transport);
 void rpmsg_interrupt(volatile struct pid_data* pid, struct pru_rpmsg_transport *transport, uint8_t *payload,
         uint16_t src, uint16_t dst, uint16_t len);
 void rpmsg_isr(volatile struct pid_data* pid, struct pru_rpmsg_transport *transport, uint8_t *payload,
@@ -141,6 +138,7 @@ void main(void) {
     /* RPMsg variables */
     struct pru_rpmsg_transport transport;
     uint16_t src, dst, len;
+//    volatile uint8_t *status;
 
     /* allow OCP master port access by the PRU so the PRU can read external memories */
     CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
@@ -258,22 +256,22 @@ int get_enc_rpm() {
 /*
  * init_rpmsg
  */
-void init_rpmsg(struct pru_rpmsg_transport *transport) {
+void init_rpmsg(struct pru_rpmsg_transport* transport) {
 	volatile uint8_t *status;
 
     /* clear the status of event MB_INT_NUMBER (the mailbox event) and enable the mailbox event */
-    CT_INTC.SICR_bit.STS_CLR_IDX = MB_INT_NUMBER;
-    CT_MBX.IRQ[MB_USER].ENABLE_SET |= 1 << (MB_FROM_ARM_HOST * 2);
+    CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+//    CT_MBX.IRQ[MB_USER].ENABLE_SET |= 1 << (MB_FROM_ARM_HOST * 2);
 
 	/* Make sure the Linux drivers are ready for RPMsg communication */
 	status = &resourceTable.rpmsg_vdev.status;
 	while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
 
     /* Initialize pru_virtqueue corresponding to vring0 (PRU to ARM Host direction) */
-    pru_virtqueue_init(&transport->virtqueue0, &resourceTable.rpmsg_vring0, &CT_MBX.MESSAGE[MB_TO_ARM_HOST], &CT_MBX.MESSAGE[MB_FROM_ARM_HOST]);
+    pru_rpmsg_init(transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
 
     /* Initialize pru_virtqueue corresponding to vring1 (ARM Host to PRU direction) */
-    pru_virtqueue_init(&transport->virtqueue1, &resourceTable.rpmsg_vring1, &CT_MBX.MESSAGE[MB_TO_ARM_HOST], &CT_MBX.MESSAGE[MB_FROM_ARM_HOST]);
+    pru_rpmsg_init(transport, &resourceTable.rpmsg_vring1, &resourceTable.rpmsg_vring0, TO_ARM_HOST, FROM_ARM_HOST);
 
     /* Create the RPMsg channels between the PRU and ARM user space using the transport structure. */
     while (pru_rpmsg_channel(RPMSG_NS_CREATE, transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
@@ -289,23 +287,19 @@ void rpmsg_interrupt(volatile struct pid_data* pid, struct pru_rpmsg_transport *
     if(__R31 & HOST_INT){
 
         /* Clear the mailbox interrupt */
-        CT_MBX.IRQ[MB_USER].STATUS_CLR |= 1 << (MB_FROM_ARM_HOST * 2);
+ //       CT_MBX.IRQ[MB_USER].STATUS_CLR |= 1 << (MB_FROM_ARM_HOST * 2);
         /* Clear the event status, event MB_INT_NUMBER corresponds to the mailbox interrupt */
-        CT_INTC.SICR_bit.STS_CLR_IDX = MB_INT_NUMBER;
+        CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
 
         /* Use a while loop to read all of the current messages in the mailbox */
-        while(CT_MBX.MSGSTATUS_bit[MB_FROM_ARM_HOST].NBOFMSG > 0){
+        //while(CT_MBX.MSGSTATUS_bit[MB_FROM_ARM_HOST].NBOFMSG > 0)
             /* Check to see if the message corresponds to a receive event for the PRU */
-            if(CT_MBX.MESSAGE[MB_FROM_ARM_HOST] == 1){
+        //    if(CT_MBX.MESSAGE[MB_FROM_ARM_HOST] == 1)
                 /* Receive the message */
                 if(pru_rpmsg_receive(transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS){
                     /* Service the new interrupt */
                     rpmsg_isr(pid, transport, payload, src, dst);
-                }
-            }
-        }
-    }
-}
+                }}}
 
 /*
  * rpmsg_isr
